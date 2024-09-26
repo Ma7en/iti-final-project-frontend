ac8c7338e5d47545d1d54e2d9baa82e9911fd892
Ma7en <m9ee9m@gmail.com>
2024-09-27 00:11:02 +0300

    edit contact

--- src/ui/contact/Contact.jsx
+++ src/ui/contact/Contact.jsx
@@ -1,77 +1,69 @@
+import React, { useState } from "react";
 
-import React, { useState } from 'react';
-import './Contact.css';
-import Hero from '../hero/Hero';
-import Footer from '../footer/Footer';
+//
+import "./Contact.css";
+
+// ui component
+import Footer from "../footer/Footer";
 const Contact = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-       
-        console.log(formData);
-     
-        setFormData({ name: '', email: '', message: '' });
+
+        // console.log(formData);
     };
 
     return (
-      <>
-       <Hero/>
-       <h1>Contact Us</h1>
-       <div className="contact-container">
-             
-           
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input 
-                        type="text" 
-                        id="name" 
-                        name="name" 
-                        value={formData.name} 
-                        onChange={handleChange} 
-                        required 
-                    />
+        <>
+            <div className="contact">
+                <div className="contact-container">
+                    <h1>Contact Us</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                // value={formData.name}
+                                // onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                // value={formData.email}
+                                // onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="message">Message</label>
+                            <textarea
+                                id="message"
+                                name="message"
+                                // value={formData.message}
+                                // onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <button className="btn" type="submit">
+                            Send Message
+                        </button>
+                    </form>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input 
-                        type="email" 
-                        id="email" 
-                        name="email" 
-                        value={formData.email} 
-                        onChange={handleChange} 
-                        required 
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message</label>
-                    <textarea 
-                        id="message" 
-                        name="message" 
-                        value={formData.message} 
-                        onChange={handleChange} 
-                        required 
-                    />
-                </div>
-                <button className='btn' type="submit">Send Message</button>
-            </form>
-        </div>
-        <Footer/>
-      </>
-        
+            </div>
+            <Footer />
+        </>
     );
 };
 
