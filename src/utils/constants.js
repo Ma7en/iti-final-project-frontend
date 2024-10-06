968efa7b7b4f22026c9f3a75728ff3a66bb24cbd
Ma7en <m9ee9m@gmail.com>
2024-10-06 20:17:53 +0300

    Run on Sٍerver

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,9 +8,12 @@ export const REFRESH_TOKEN = "refresh";
 //
 // import UserData from "../views/plugin/UserData";
 
-export const API_BASE_URL = "http://127.0.0.1:8000/api/v1/";
-export const SERVER_URL = "http://127.0.0.1:8000";
-export const CLIENT_URL = "http://localhost:5173";
+// export const API_BASE_URL = "http://127.0.0.1:8000/api/v1/";
+export const API_BASE_URL = "https://m9ee9m2.pythonanywhere.com/api/v1/";
+// export const SERVER_URL = "http://127.0.0.1:8000";
+export const SERVER_URL = "https://m9ee9m2.pythonanywhere.com";
+// export const CLIENT_URL = "http://localhost:5173";
+export const CLIENT_URL = "https://homeverse-1.vercel.app";
 export const PAYPAL_CLIENT_ID = "test";
 export const CURRENCY_SIGN = "$";
 // export const userId = UserData()?.user_id;
